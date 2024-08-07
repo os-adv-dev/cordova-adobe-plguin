bf9e8acc07e22e6e8256e5b3a8f6ed507217b6c3
pcamilojunior <paulo.camilo.junior@outsystems.com>
2024-08-07 14:11:06 +0200

    set the new functionalitites to adobe SDK, change the AdobeMobileOlugin javascript, adn send the iOS source code

--- src/ios/AdobeMobilePlugin.swift
+++ src/ios/AdobeMobilePlugin.swift
@@ -9,29 +9,34 @@ import AEPEdgeIdentity
 
     @objc(configureWithAppID:)
     func configureWithAppID(command: CDVInvokedUrlCommand) {
-        guard let applicationID = command.argument(at: 0) as? String, !applicationID.isEmpty else {
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "You need to pass the application ID to the Adobe SDK initialization")
-            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-            return
-        }
-
-        MobileCore.registerExtensions([Identity.self, Edge.self, Consent.self, Lifecycle.self], {
-           MobileCore.configureWith(appId: applicationID)
+        DispatchQueue.global().async {
+            guard let applicationID = command.argument(at: 0) as? String, !applicationID.isEmpty else {
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "You need to pass the application ID to the Adobe SDK initialization")
+                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+                return
+            }
             
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Adobe SDK is initialized with success!")
-            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-        })
+            MobileCore.registerExtensions([Identity.self, Edge.self, Consent.self, Lifecycle.self], {
+                MobileCore.configureWith(appId: applicationID)
+                
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Adobe SDK is initialized with success!")
+                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            })
+        }
     }
 
     @objc(getConsents:)
     func getConsents(command: CDVInvokedUrlCommand) {
-        Consent.getConsents { consents, error in
-            guard error == nil, let consents = consents else { return }
-            guard let jsonData = try? JSONSerialization.data(withJSONObject: consents, options: .prettyPrinted) else { return }
-            guard let jsonStr = String(data: jsonData, encoding: .utf8) else { return }
-            print("---- Consent.getConsents :: "+jsonStr)
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: jsonStr)
-            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        DispatchQueue.global().async {
+            Consent.getConsents { consents, error in
+                
+                guard error == nil, let consents = consents else { return }
+                guard let jsonData = try? JSONSerialization.data(withJSONObject: consents, options: .prettyPrinted) else { return }
+                guard let jsonStr = String(data: jsonData, encoding: .utf8) else { return }
+                
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: jsonStr)
+                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            }
         }
     }
 
@@ -39,7 +44,7 @@ import AEPEdgeIdentity
     func lifecycleStart(command: CDVInvokedUrlCommand) {
         DispatchQueue.global().async {
             MobileCore.lifecycleStart(additionalContextData: nil)
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Success lifecycleStart")
             self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         }
     }
@@ -48,7 +53,7 @@ import AEPEdgeIdentity
     func lifecyclePause(command: CDVInvokedUrlCommand) {
         DispatchQueue.global().async {
             MobileCore.lifecyclePause()
-            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Success lifecyclePause")
             self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
         }
     }
