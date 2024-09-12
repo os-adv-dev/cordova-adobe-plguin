3b8f958abaff90363b973095b00582a3fd82527d
pcamilojunior <paulo.camilo.junior@outsystems.com>
2024-09-12 15:00:05 +0200

    fix GSON to convert Consents

--- src/android/AdobeMobilePlugin.java
+++ src/android/AdobeMobilePlugin.java
@@ -34,6 +34,7 @@
  import java.util.Iterator;
  import java.util.List;
  import java.util.Map;
+ import com.google.gson.Gson;
  
  import com.adobe.marketing.mobile.LoggingMode;
  import com.adobe.marketing.mobile.Messaging;
