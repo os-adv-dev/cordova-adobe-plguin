75f07209b903532d9156b12a87e9694268232619
pcamilojunior <paulo.camilo.junior@outsystems.com>
2024-10-29 15:45:35 +0100

    removed the documentation on top of the main file java

--- src/android/AdobeMobilePlugin.java
+++ src/android/AdobeMobilePlugin.java
@@ -1,15 +1,4 @@
-/*
- Copyright 2020 Adobe. All rights reserved.
- This file is licensed to you under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License. You may obtain a copy
- of the License at http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software distributed under
- the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- OF ANY KIND, either express or implied. See the License for the specific language
- governing permissions and limitations under the License.
- */
-
- package com.adobe.marketing.mobile.cordova;
+package com.adobe.marketing.mobile.cordova;
 
 
  import com.adobe.marketing.mobile.AdobeCallbackWithError;
@@ -158,7 +147,6 @@
  
      private void updateIdentities(final JSONArray args, final CallbackContext callbackContext) {
         try {
-
             String identityKey = args.getString(0);
             String identityValue = args.getString(1);
             String authenticatedState = args.getString(2);
